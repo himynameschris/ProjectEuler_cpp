52fd5bae50a531efbdc96fe052266370329254a6
Chris Hannon <himynameschris@gmail.com>
2012-07-06 21:26:37 -0400

    Solved: Problem 1 using for loop and % for remainder

--- problems/problem1.cpp
+++ problems/problem1.cpp
@@ -18,5 +18,19 @@ bool Problem1::init()
 
 void Problem1::run()
 {
+	int sum = 0;
+
+	for(int i = 0; i < 1000; i++)
+	{
+
+		if(i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+			std::cout << "Natural number found: " << i << " Running Total: " << sum << std::endl;
+		}
+
+	}
+
+	std::cout << "Done! The sum of all natural numbers below 1000 that are multiples of 3 or 5 is: " << sum;
 
 }
