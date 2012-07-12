f496f395215bbee2f84913579dcb0629ebca50fb
Chris Hannon <himynameschris@gmail.com>
2012-07-12 00:59:41 -0400

    Issue: add missing break statements in switch

--- problemfactory.cpp
+++ problemfactory.cpp
@@ -10,24 +10,34 @@ Problem * ProblemFactory::create(int choice)
 	{
 	case 1:
 		p = (Problem *)new Problem1();
+		break;
 	case 2:
 		p = (Problem *)new Problem2();
+		break;
 	case 3:
 		p = (Problem *)new Problem3();
+		break;
 	case 4:
 		p = (Problem *)new Problem4();
+		break;
 	case 5:
 		p = (Problem *)new Problem5();
+		break;
 	case 6:
 		p = (Problem *)new Problem6();
+		break;
 	case 7:
 		p = (Problem *)new Problem7();
+		break;
 	case 8:
 		p = (Problem *)new Problem8();
+		break;
 	case 9:
 		p = (Problem *)new Problem9();
+		break;
 	case 10:
 		p = (Problem *)new Problem10();
+		break;
 	}
 
 	if(p && p->init())
