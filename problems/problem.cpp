06179274aa75015080be5b2576c611911d9318ea
Chris Hannon <himynameschris@gmail.com>
2012-07-24 20:54:20 -0400

    Update Project: add problem template to save time when adding a new problem, excluded from the VS project

---
+++ problems/problem.cpp
@@ -0,0 +1,29 @@
+#include "StdAfx.h"
+#include "../problems.h"
+
+Problem13::Problem13()
+{
+
+}
+
+Problem13::~Problem13()
+{
+
+}
+
+bool Problem13::init()
+{
+	return true;
+}
+
+void Problem13::run()
+{
+	/*
+	*/
+
+	//method: 
+	
+	
+	std::cout << ": " << std::endl;
+	
+}
